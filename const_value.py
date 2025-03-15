246997fbde9bdb9ffbe4b0d686ce93e44edc50c8
kekusuko <kekulovaa@inbox.ru>
2025-03-15 23:39:59 +0500

    Update const_value.py
    +color

--- const_value.py
+++ const_value.py
@@ -16,3 +16,10 @@ game_over = 0
 level = 1
 max_levels = 2
 main_menu = True
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (200, 200, 200)
