301989e175e00c1ca4ed5021800dadf078d7f410
kekusuko <kekulovaa@inbox.ru>
2025-03-19 10:20:07 +0500

    Update const_value.py

--- const_value.py
+++ const_value.py
@@ -2,10 +2,8 @@ import pygame
 
 pygame.init()
 
-bg_img = pygame.image.load('img/bg.png')
-
 WIDTH, HEIGHT = 1000, 800
-FPC = 60
+FPS = 60
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("I Hate This Game")
@@ -14,7 +12,6 @@ clock = pygame.time.Clock()
 tile_size = 50
 game_over = 0
 level = 1
-max_levels = 2
 main_menu = True
 
 WHITE = (255, 255, 255)
@@ -23,3 +20,4 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 GRAY = (200, 200, 200)
+DARK_GRAY = (169, 169, 169)
