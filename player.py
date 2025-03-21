c21fc1a20b60c7c7dba09cdb878d6da8cf46e512
remalQ <remalqmain@gmail.com>
2025-03-21 12:18:49 +0500

    Fixed file logistics

--- player.py
+++ player.py
@@ -77,12 +77,14 @@ class Player:
         self.images_left = []
         self.index = 0
         self.counter = 0
+
         for num in range(1, 6):
             img_right = pygame.image.load(f'img/frame{num}.png')
             img_right = pygame.transform.scale(img_right, (40, 60))
             img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
             self.images_left.append(img_left)
+
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
